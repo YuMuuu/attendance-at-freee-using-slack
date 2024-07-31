47c74746cde825449320a94555f734c7918c8b85
YuMuuu <curelemonade15@gmail.com>
2024-08-01 03:33:04 +0900

    wip

---
+++ src/utill/ClockInStatus.ts
@@ -0,0 +1,10 @@
+// 打刻可能種別
+type ClockInStatus =
+  | 'buturi_in'
+  | 'buturi_out'
+  | 'remote_in'
+  | 'remote_out'
+  | 'kyukei_in'
+  | 'kyukei_out'
+
+export type { ClockInStatus }
---
+++ src/utill/freeeUtill.ts
@@ -0,0 +1,36 @@
+import { ClockInStatus } from "./ClockInStatus";
+
+type email = string
+type employeeId = number
+
+
+
+//todo: 従業員一覧を取得し、email二関連する従業員IDを取得するmapを作成する
+//memo: 入社/退社した人がいた場合に同対応する？再取得actionを作るか一定期間で再取得するようにする？
+const employeeIdMap = new Map<email, employeeId>();
+
+
+
+
+const companyId = 111 //固定値なのでenvで取る
+async function registerClockIn(employeeId: employeeId, clockInStatus: ClockInStatus): Promise<void> {
+
+    const req = await fetch(`https://api.freee.co.jp/api/v1/employees/${employeeId}/time_clocks`,
+        {
+            body: JSON.stringify({ company_id: companyId, type: clockInStatus }),
+        }
+    )
+
+    const resp = await req
+    if(!resp.ok) {
+        //resp.body.errors[0].messages をエラー内容にして変える
+        throw new Error("エラーが発生しました");
+
+    }
+}
+
+
+
+
+
+
